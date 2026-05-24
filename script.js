ff7ce4d83d7cdfa61abe638d4ca83fd7c67e2014
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-05-24 13:51:12 +0500

    update

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
- alert("Hello user");
- alert("Welcome to our website");
+
+//  Variable
+ 
+  var name = prompt("Enter your name")
+  var age = prompt("Enter your age");
+  var country = prompt("Enter your country");
+   if(age >= 18  && country == "Pakistan"){
+    alert("Hello Citizen");
+   }
+   else if(age < 18  && country != "Pakistan"){
+    alert("you are not allowed");
+   }
+   else{
+    alert("Access Denied");
+   }
+   
